c7f4ec4f1533138b05c8e79f78b878299b7df5c7
deepwjp <deepwjp@outlook.com>
2020-11-02 14:21:13 +0800

    剑指 Offer 66. 构建乘积数组

--- 剑指offer/剑指 Offer 66. 构建乘积数组.go
+++ 剑指offer/剑指 Offer 66. 构建乘积数组.go
@@ -14,6 +14,13 @@ package 剑指offer
 
 所有元素乘积之和不会溢出 32 位整数
 a.length <= 100000 */
+
+/*
+初始化：数组 BB ，其中 B[0] = 1B[0]=1 ；辅助变量 tmp = 1tmp=1 ；
+计算 B[i]B[i] 的 下三角 各元素的乘积，直接乘入 B[i]B[i] ；
+计算 B[i]B[i] 的 上三角 各元素的乘积，记为 tmptmp ，并乘入 B[i]B[i] ；
+返回 BB 。
+*/
 func constructArr(a []int) []int {
 	if len(a) == 0 {
 		return a
@@ -22,11 +29,11 @@ func constructArr(a []int) []int {
 	b := make([]int, len(a))
 	b[0] = 1
 	for i := 1; i < len(a); i++ {
-		b[i] = b[i-1] * a[i-1]
+		b[i] = b[i-1] * a[i-1] // 下三角
 	}
 	for i := len(a) - 2; i >= 0; i-- {
-		tmp *= a[i+1]
-		b[i] *= tmp
+		tmp *= a[i+1] // 上三角
+		b[i] *= tmp   // 下三角 * 上三角
 	}
 	return b
 }
