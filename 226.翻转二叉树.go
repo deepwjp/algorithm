a8f38e7e28c2570681f3af121aa4af1e713677b4
deepwjp <deepwjp@outlook.com>
2020-11-03 13:02:44 +0800

    [226] 翻转二叉树

---
+++ 226.翻转二叉树.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=226 lang=golang
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Right, root.Left = root.Left, root.Right
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+
+// @lc code=end
