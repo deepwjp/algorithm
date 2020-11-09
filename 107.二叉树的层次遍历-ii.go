66a2fb748290baafaa790e2447f5d323154ca326
deepwjp <deepwjp@outlook.com>
2020-11-09 14:38:27 +0800

    107. 二叉树的层次遍历 II  层次遍历

---
+++ 107.二叉树的层次遍历-ii.go
@@ -0,0 +1,44 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=107 lang=golang
+ *
+ * [107] 二叉树的层次遍历 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrderBottom(root *TreeNode) (res [][]int) {
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	for i := 0; len(queue) > 0; i++ {
+		res = append(res, []int{})
+		p := []*TreeNode{}
+		for j := 0; j < len(queue); j++ {
+			node := queue[j]
+			res[i] = append(res[i], node.Val)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		queue = p
+	}
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-1-i] = res[len(res)-1-i], res[i]
+	}
+	return
+}
+
+// @lc code=end
